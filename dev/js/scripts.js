a0dd924cacfea1c8206333ea6c7ffa13106bec93
ecarhuapoma <81769655+ecarhuapoma@users.noreply.github.com>
2021-12-07 16:49:01 -0500

    updated

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -10,72 +10,42 @@ gsap.registerPlugin(GSDevTools, MorphSVGPlugin, EaselPlugin, ExpoScaleEase, Roug
 
 const mainTL = gsap.timeline();
 
-mainTL.set("#preloader",{scale:1.5, x:0, y:-1700});
-mainTL.set(".stars",{scale:2,x:-150, y:-1400});
-mainTL.set("#comet",{scale:2,x:-600, y:200});
-mainTL.set(".planetStars",{scale:1.2,x:-25, y:-800});
-// mainTL.set(".logo-stars",{alpha:1});
+mainTL.set("#rps-animation",{scale:1});
+mainTL.set(".noses",{alpha:0});
+mainTL.set(".type",{alpha:0,y:20});
 
-mainTL.set(".stars-1",{alpha:1});
-
-// twinkle();
-
-// function twinkle(){
-//     const tl=gsap.timeline();
-//     tl.fromTo(".stars-1",{duration:0.01, alpha:1},{stagger:0.1, alpha:.3});
-    
-// // tl.fromTo(".logo-stars",{duration:0.1, alpha:1},{stagger:0.3, alpha:.3});
-// }
-
-
-gsap.timeline({onStart:mainTL.to("#comet",{x:700, y:-600, ease:"power3.in",duration:4,autoAlpha:.5})});
-
-
-function intro(){
-const tl=gsap.timeline();
-
-
-// tl.set(".finalLogo",{autoAlpha:1});
-
-// tl.to(".stars",{scale:1.99, duration:.5, yoyo:true, repeat:5});
-// tl.to("#comet",{x:700, y:-600, ease:"power4.in",duration:4,autoAlpha:.5});
-
-
-// tl.to("#comet",{y:600,duration:2},"sametime");
-tl.to(".stars",{y:2000,duration:5,stagger:-.4,ease:"power4.inOut"},"-=3.5");
-tl.to(".planetStars",{x:-25,y:-42,duration:2,ease:"power1.out"},"-=3");
-
-return tl;
-}
-
-function planetBuilding(){
+function petDraw(){
     const tl=gsap.timeline();
-    
-    tl.set(".logoParts",{autoAlpha:1});
-    tl.from(".logoParts #Vector_14",{drawSVG:"100% 100%", ease:"power4.out", duration:3});
-    tl.from(".logoParts #Vector_16",{drawSVG:"100% 100%", duration:.5},"-=1.3");
-    tl.from(".logoParts #Vector_15",{drawSVG:"100% 100%", duration:.5}, "-=.8");
-    tl.to(".orbit-word",{autoAlpha:1, duration:1},"-=.5");
+
+    tl.from(".pets",{stagger:0.1,drawSVG:"100% 100%", ease:"power4.out", duration:3});
+    tl.to(".noses",{autoAlpha:1, duration:.2},"-=2");
+
     return tl;
 }
 
-function logoTwinkle(){
-    const tl=gsap.timeline({repeat:-1, yoyo:true});
-    tl.fromTo(".logo-stars",{duration:.1, alpha:.3},{stagger:.3, alpha:1});
+function typeAppear(){
+    const tl=gsap.timeline();
+
+    tl.to(".type",{autoAlpha:1,stagger:0.1,y:0,duration:.5,ease:"power3.out"});
+    // tl.to(".type",{autoAlpha:1},"-=2");
+
+    return tl;
 }
 
-function twinkle(){
-    const tl=gsap.timeline({repeat:-1, yoyo:true});
-    // tl.fromTo(".stars-1",{duration:.5, alpha:.3},{stagger:0.3, alpha:1});
-}
+// function twinkle(){
+//     const tl=gsap.timeline({repeat:-1, yoyo:true});
+//     // tl.fromTo(".stars-1",{duration:.5, alpha:.3},{stagger:0.3, alpha:1});
+// }
 
-logoTwinkle();
-twinkle();
+// logoTwinkle();
+// twinkle();
 
-mainTL.add(intro(),"-=2")
-.add(planetBuilding(),"-=1");
+// mainTL.add(intro(),"-=2")
+// .add(planetBuilding(),"-=1");
 
 
+mainTL.add(petDraw())
+.add(typeAppear(),"-=3");
 
 
 GSDevTools.create();
